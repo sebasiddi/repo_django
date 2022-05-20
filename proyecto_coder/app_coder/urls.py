2416fe55f080e6c7e415ed98971ec24b1f133434
Sebastian Siddi <ssiddi@gmail.com>
2022-05-20 16:18:40 -0300

    Segunda actualización con template

--- proyecto_coder/app_coder/urls.py
+++ proyecto_coder/app_coder/urls.py
@@ -2,6 +2,7 @@ from django.urls import URLPattern, path
 from . import views
 
 urlpatterns = [
+        path("inicio",views.inicio),
         path("Cursos", views.curso),
         path("alta_curso/<nombre>", views.alta_curso)
 
--- proyecto_coder/app_coder/views.py
+++ proyecto_coder/app_coder/views.py
@@ -1,15 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from app_coder.models import Curso
+from django.template import loader
 # Create your views here.
 
-
+"""
 def curso(request):
     cursos = Curso.objects.all()
     return HttpResponse(cursos)
-
+"""
 def alta_curso(request,nombre):
     curso = Curso(nombre=nombre,camada=21515)
     curso.save()
     texto = f"se guardó en la DB el curso {curso.nombre} camada {curso.camada}"
     return HttpResponse(texto)
+
+def inicio(request):
+    return render(request,"template/plantilla.html")
+
+def curso(request):
+    cursos = Curso.objects.all()
+    dicc = {"cursos":cursos}
+    plantilla = loader.get_template("template/plantilla.html")
+    documento=plantilla.render(dicc)
+    return HttpResponse(documento)
+
+
+
+
